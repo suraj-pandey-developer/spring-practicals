2e5888025a090cb15fe6b3a94feb3705d33a2d79
suraj-pandey-developer <officialsurajpandey55@gmail.com>
2023-10-19 02:03:24 +0530

    add: custom query and methods

---
+++ custom_query/src/main/java/com/custom/query/repository/UserRepository.java
@@ -0,0 +1,23 @@
+package com.custom.query.repository;
+
+import com.custom.query.model.User;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface UserRepository extends CrudRepository<User, Integer> {
+    // hpa query of sort form it will converted automatically and then convert back to sql command.
+    // Hence take time to execute.
+    @Query(value = "From User")
+    List<User> getAllUserHql() ;
+
+    // native query annotation is required so that it can understand diff of hql and sql query type
+    @Query(value = "select * from user_master", nativeQuery = true)
+    List<User> getAllUserSql();
+
+    @Query(value = "From User where country = :cname")
+    List<User> getAllUserByCountry(String cname);
+}
